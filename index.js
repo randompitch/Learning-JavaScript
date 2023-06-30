224a180acc5c760181f29d224f75259ada641789
Megha Chauhan <megha.chauhan376@gmail.com>
2023-06-30 15:45:45 +0530

    altered the function to allow the usage of button

--- index.js
+++ index.js
@@ -1,18 +1,24 @@
-const users = [
-    { fN: "Megha", lN: "Chauhan", age: 19 },
-    { fN: "Richa", lN: "Chauhan", age: 23 },
-    { fN: "Taylor", lN: "Swift", age: 33 },
-    { fN: "Harry", lN: "Styles", age: 29}  
-];
+function mapFilRed(){
+    const users = [
+        { fN: "Megha", lN: "Chauhan", age: 19 },
+        { fN: "Richa", lN: "Chauhan", age: 23 },
+        { fN: "Taylor", lN: "Swift", age: 33 },
+        { fN: "Harry", lN: "Styles", age: 29}  
+    ];
 
-const output = users.filter((x) => x.age<27).map((x) => x.fN)
-console.log(output);
-
-const vut = users.reduce(function(acc, curr){
-    if(curr.age < 27){
-        acc.push(curr.fN);
-    }
-    return acc;df
-}, []);
-
-console.log(vut);
+    console.log("This shows the working of map(), filter() and reduce()");
+    console.log("Print the names of users who are less than 27 years of age: (using Filter and Map)");
+    
+    const output = users.filter((x) => x.age<27).map((x) => x.fN)
+    console.log(output);
+    
+    const vut = users.reduce(function(acc, curr){
+        if(curr.age < 27){
+            acc.push(curr.fN);
+        }
+        return acc;df
+    }, []);
+    
+    console.log("Print the names of users who are less than 27 years of age: (using only Reduce)");
+    console.log(vut);
+}
