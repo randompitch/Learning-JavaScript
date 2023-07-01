532f1337717b5d385d5b2baf4ed334bde9a87690
Megha Chauhan <megha.chauhan376@gmail.com>
2023-07-01 17:08:10 +0530

    Added an Upcounter

--- index.js
+++ index.js
@@ -5,7 +5,8 @@ function mapFilRed(){
         { fN: "Taylor", lN: "Swift", age: 33 },
         { fN: "Harry", lN: "Styles", age: 29}  
     ];
-
+    
+    alert("Open Console");
     console.log("This shows the working of map(), filter() and reduce()");
     console.log("Print the names of users who are less than 27 years of age: (using Filter and Map)");
     
@@ -21,4 +22,19 @@ function mapFilRed(){
     
     console.log("Print the names of users who are less than 27 years of age: (using only Reduce)");
     console.log(vut);
+}
+let mySW;
+function counter(){
+    alert("Open Console");
+    let timer = 0;
+    function sWatcho(){
+        timer++;
+        //console.clear();
+        console.log(timer);
+    }
+    mySW = setInterval(sWatcho,1000);
+}
+function stopCounter(){
+    clearInterval(mySW);
+    console.log("Terminating counter");
 }
